d17a13e02cb83fc4fe166b6f5a760ea0d0e42c81
Puneet Goyal <puneetgoyal08@gmail.com>
2014-03-04 11:47:51 +0530

    Added the login screen and API to perform online post requests.

---
+++ CookShare2/LoginScreen.h
@@ -0,0 +1,13 @@
+//
+//  LoginScreen.h
+//  CookShare2
+//
+//  Created by Puneet Goyal on 21/02/14.
+//  Copyright (c) 2014 Puneet Goyal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginScreen : UIViewController
+
+@end
---
+++ CookShare2/LoginScreen.m
@@ -0,0 +1,38 @@
+//
+//  LoginScreen.m
+//  CookShare2
+//
+//  Created by Puneet Goyal on 21/02/14.
+//  Copyright (c) 2014 Puneet Goyal. All rights reserved.
+//
+
+#import "LoginScreen.h"
+
+@interface LoginScreen ()
+
+@end
+
+@implementation LoginScreen
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
